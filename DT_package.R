88bc5de244d29fb25dd165fbb2aa6bae44fb9633
Nguyễn Ngọc Bình <47907431+nguyenngocbinh@users.noreply.github.com>
2019-12-28 09:38:44 +0700

    create

---
+++ DT_package.R
@@ -0,0 +1,11 @@
+create_dt <- function(x){
+  DT::datatable(x,
+                extensions = 'Buttons',
+                options = list(dom = 'Blfrtip',
+                               buttons = c('copy', 'csv', 'excel', 'pdf', 'print'),
+                               lengthMenu = list(c(10,25,50,-1),
+                                                 c(10,25,50,"All"))))
+}
+
+create_dt(iris)
+
