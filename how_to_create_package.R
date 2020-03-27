e5be16846ac50aa0d19b8c55758cdab1f31c5caa
Nguyễn Ngọc Bình <47907431+nguyenngocbinh@users.noreply.github.com>
2020-03-27 16:19:24 +0700

    update create data

--- how_to_create_package.R
+++ how_to_create_package.R
@@ -72,34 +72,25 @@ devtools::build_vignettes()
 # names(hmeq) <- tolower(names(hmeq))
 
 usethis::use_data_raw("hmeq")
-usethis::use_data("hmeq")
+usethis::use_data(hmeq)
 
-#' This is data to be included in my package
-#' @name hmeq
+#' hmeq
 #' @docType data
+#' @name hmeq
 #' @usage data(hmeq)
 #' @author Nguyen Ngoc Binh \email{nguyenngocbinhneu@@gmail.com}
 #' @references \url{https://codeload.github.com/Carl-Lejerskar/HMEQ/zip/master}
-#' @keywords data
-#' \itemize{
-#'   \item BAD. 1 = client defaulted on loan, 0 = loan repaid
-#'   \item LOAN. Amount of the loan request
-#'   \item MORTDUE. Amount due on existing mortgage
-#'   \item VALUE. Value of current property
-#'   \item REASON. DebtCon = debt consolidation HomeImp = home improvement
-#'   \item JOB. Six occupational categories
-#'   \item YOJ. Years at present job
-#'   \item DEROG. Number of major derogatory reports
-#'   \item DELINQ. Number of delinquent credit lines
-#'   \item CLAGE. Age of oldest trade line in months
-#'   \item NINQ. Number of recent credit lines
-#'   \item CLNO. Number of credit lines
-#'   \item DEBTINC. Debt-to-income ratio
-#' }
+#' @keywords datasets
 #' @examples
-#' data('hmeq')
+#' data(hmeq)
+NULL
+
 
 #=============================================================================
 # Build package
 devtools::check()
 devtools::build()
+
+#=============================================================================
+# Depends:
+#   R (>= 3.1.0)
